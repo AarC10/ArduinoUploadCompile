2b41733387e1c7dce3095977ef26c6c2acb995b6
Aaron Chan <aarchan108@gmail.com>
2022-05-29 14:27:42 -0400

    Variables for directory, fqbn and port

--- main.c
+++ main.c
@@ -6,13 +6,18 @@
 
 #include <stdio.h>
 
-int main(int argc, char* argv) {
+int main(int argc, char** argv) {
     if (argc != 5) {
-        printf("Requires an arduino directory, fqbn and port");
+        printf("Requires an arduino directory, fqbn and port\n");
         return 1;
     }
 
-    printf("Success");
+    char* directory = argv[2];
+    char* fqbn = argv[3];
+    char* port = argv[4];
+
+
+    printf("Success\n");
 
     return 0;
 }
