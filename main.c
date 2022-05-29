b84197c4a689288e58c25f40b7107f14bee8d67b
Aaron Chan <aarchan108@gmail.com>
2022-05-29 14:44:48 -0400

    Commands created

--- main.c
+++ main.c
@@ -7,15 +7,20 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-    if (argc != 5) {
+    if (argc != 4) {
         printf("Requires an arduino directory, fqbn and port\n");
         return 1;
     }
 
-    char* directory = argv[2];
-    char* fqbn = argv[3];
-    char* port = argv[4];
+    char* directory = argv[1];
+    char* fqbn = argv[2];
+    char* port = argv[3];
 
+    char* compileCommand;
+    char* uploadCommand;
+
+    if (0 > asprintf(&compileCommand, "arduino-cli compile -b arduino:avr:%s %s\n", directory, fqbn)) return 2;
+    if (0 > asprintf(&uploadCommand, "arduino-cli upload %s -b arduino:avr:%s -p /dev/%s", directory, fqbn, port)) return 2;
 
     printf("Success\n");
 
